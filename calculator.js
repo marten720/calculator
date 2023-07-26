42f1679bb9328ad8fa872e50877130a32ca67933
marten720 <137708101+marten720@users.noreply.github.com>
2023-07-26 17:51:13 +0300

    Changes were made in calculator.js. The screen now responds to the buttons being clicked. Started working on making the operator buttons work, but stopped. Ensured that the height of the screen is always the same unless the numbers are large enough that they need to wrap on multiple lines.

--- calculator.js
+++ calculator.js
@@ -1,17 +1,51 @@
+const curr = document.querySelector(".curr");
+const last = document.querySelector(".last");
+
 // all of the necessary math functions
-function add(a, b) {
-    return Number(a) + Number(b);
+function sum() {
+    last.textContent = Number(curr.textContent) + Number(last.textContent) + " +";
+
+    curr.textContent = Number(curr.textContent) + Number(last.textContent);
 }
 
-function subtract(a, b) {
-    return Number(a) - Number(b);
+function subtract() {
+    curr.textContent = Number(curr.textContent) - Number(last.textContent);
 }
 
-function multiply(a, b) {
-    return Number(a) * Number(b);
+function multiply() {
+    curr.textContent = Number(curr.textContent) * Number(last.textContent);
 }
 
-function divide(a, b) {
-    return Number(a) / Number(b);
+function divide() {
+    curr.textContent = Number(curr.textContent) / Number(last.textContent);
 }
 
+function power() {
+    curr.textContent = Number(curr.textContent) ** Number(last.textContent);
+}
+
+function calculate() {
+    if (last.textContent != "") {
+        
+    }
+}
+
+// other calculator buttons
+function allClear() {
+    curr.textContent = "0";
+    last.textContent = "";
+}
+
+function del() {
+    if (curr.textContent.length > 0)
+        curr.textContent = curr.textContent.slice(0, -1);
+
+    if (curr.textContent.length === 0)
+        curr.textContent = "0";
+}
+
+function add(num) {
+    while (curr.textContent[0] === "0")
+        curr.textContent = curr.textContent.slice(1);
+    curr.textContent += num;
+}
